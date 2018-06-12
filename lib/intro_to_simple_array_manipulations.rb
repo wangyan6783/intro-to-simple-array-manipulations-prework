50e5e5fc9190dec6093a2d3a5f5a684af72a0f1c
Yan Wang <wangyan6783@gmail.com>
2018-06-12 15:44:08 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,34 +6,12 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-describe "#using_unshift" do 
-  before(:each) do 
-    neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-    @new_neighborhood = "Brooklyn Heights"
-    @updated_array = using_unshift(neighborhoods_in_northwest_brooklyn, @new_neighborhood)
-  end
-  
-  it "takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method" do
-    expect(@updated_array.first).to eq(@new_neighborhood)
-  end
-
-  it "increases the length of the array" do 
-    expect(@updated_array.size).to eq(6)
-  end
+def using_pop(array)
+  array.pop
 end
 
-describe "#using_pop" do
-  before(:each) do 
-    @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-    @deleted_string = using_pop(@great_hits_of_the_nineties)
-  end
-  it "takes in argument of an array and uses the pop method to remove the last element from the array and return that element" do 
-    expect(@deleted_string).to eq("Losing My Religion")
-  end
-
-  it "decreases the length of the array by 1" do 
-    expect(@great_hits_of_the_nineties.size).to eq(5)
-  end
+def pop_with_args(array)
+  
 end
 
 describe "#pop_with_args" do

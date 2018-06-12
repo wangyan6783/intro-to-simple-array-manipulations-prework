bf0d57b9da1f823cd097ad9a5489d30c5aa2b36a
Yan Wang <wangyan6783@gmail.com>
2018-06-12 16:34:21 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,20 +30,8 @@ def using_insert(array, element)
   array(4, element)
 end
 
-describe "#using_insert" do 
-  before(:each) do 
-    @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-    @another_esoteric_language = "Malbolge"
-    @new_array = using_insert(@list_of_esoteric_programming_languages, @another_esoteric_language)
-  end
-
-  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
-    expect(@new_array[4]).to eq(@another_esoteric_language)
-  end
-
-  it "increases the length of the array" do 
-    expect(@new_array.length).to eq(8)
-  end
+def using_uniq()
+  
 end
 
 describe "#using_uniq" do 

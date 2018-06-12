eb6898ff4b3a8fbdc6dc313d345ebc4b07340a35
Yan Wang <wangyan6783@gmail.com>
2018-06-12 15:57:28 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,19 +26,8 @@ def using_concat(arr1, arr2)
   arr1.concat(arr2)
 end
 
-describe "#using_concat" do 
-  before(:each) do 
-    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["mario kart", "flatiron school"]
-    @all_my_favs = using_concat(@my_favorite_things, more_favs)
-  end
-   it "takes in two arguments of two different arrays and uses the concat method to add the contents of the second array to the first" do 
-    expect(@all_my_favs).to eq(["raindrops on roses", "whiskers on kittens", "mario kart", "flatiron school"])
-   end
-
-   it "increases the length of the first array" do 
-    expect(@my_favorite_things.length).to be > 2
-   end
+def using_insert(array, element)
+  
 end
 
 describe "#using_insert" do 
